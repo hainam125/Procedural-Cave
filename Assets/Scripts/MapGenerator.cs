f2616067421e7ea1b39ac3788c9c3993adfc4561
NamNH <hainam125@gmail.com>
2021-04-10 22:05:34 +0700

    Detecting Regions

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -29,6 +29,8 @@ public class MapGenerator : MonoBehaviour {
             SmoothMap();
         }
 
+        ProcessMap();
+
         int borderSize = 1;
         int[,] borderMap = new int[width + borderSize * 2, height + borderSize * 2];
 
@@ -82,7 +84,7 @@ public class MapGenerator : MonoBehaviour {
         int wallCount = 0;
         for (int x = gridX - 1; x <= gridX + 1; x++) {
             for (int y = gridY - 1; y <= gridY + 1; y++) {
-                if (x >= 0 && x < width && y >= 0 && y < height) {
+                if (IsInMapRange(x, y)) {
                     if (x != gridX || y != gridY) wallCount += map[x, y];
                 }
                 else {
@@ -91,5 +93,86 @@ public class MapGenerator : MonoBehaviour {
             }
         }
         return wallCount;
+    }
+
+    private void ProcessMap() {
+        var wallRegions = GetRegions(1);
+        var wallThreshold = 50;
+        foreach(var wallRegion in wallRegions) {
+            if(wallRegion.Count < wallThreshold) {
+                foreach(var tile in wallRegion) {
+                    map[tile.tileX, tile.tileY] = 0;
+                }
+            }
+        }
+
+        var roomRegions = GetRegions(0);
+        var roomThreshold = 50;
+        foreach (var roomRegion in roomRegions) {
+            if (roomRegion.Count < roomThreshold) {
+                foreach (var tile in roomRegion) {
+                    map[tile.tileX, tile.tileY] = 1;
+                }
+            }
+        }
+    }
+
+    private List<List<Coord>> GetRegions(int tileType) {
+        var regions = new List<List<Coord>>();
+        var mapFlags = new int[width, height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if(mapFlags[x,y]==0 && map[x, y] == tileType) {
+                    var newRegion = GetRegionTiles(x, y);
+                    regions.Add(newRegion);
+                    foreach (var tile in newRegion) mapFlags[tile.tileX, tile.tileY] = 1;
+                }
+
+            }
+        }
+
+        return regions;
+    }
+
+    private List<Coord> GetRegionTiles(int startX, int startY) {
+        var tiles = new List<Coord>();
+        var mapFlags = new int[width, height];
+        var tileType = map[startX, startY];
+
+        var queue = new Queue<Coord>();
+        queue.Enqueue(new Coord(startX, startY));
+        mapFlags[startX, startY] = 1;
+
+        while (queue.Count > 0) {
+            Coord tile = queue.Dequeue();
+            tiles.Add(tile);
+
+            for (int x = tile.tileX - 1; x <= tile.tileX + 1; x++) {
+                for (int y = tile.tileY - 1; y <= tile.tileY + 1; y++) {
+                    if (IsInMapRange(x, y) && (y == tile.tileY || x == tile.tileX)) {
+                        if (mapFlags[x, y] == 0 && map[x, y] == tileType) {
+                            mapFlags[x, y] = 1;
+                            queue.Enqueue(new Coord(x, y));
+                        }
+                    }
+                }
+            }
+        }
+
+        return tiles;
+    }
+
+    private bool IsInMapRange(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    private struct Coord {
+        public int tileX, tileY;
+
+        public Coord(int x, int y) {
+            tileX = x;
+            tileY = y;
+        }
     }
 }
